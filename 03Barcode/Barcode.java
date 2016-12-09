73c6635dde4940463aafcdaea2ff6b3e1515facc
Charles Weng School <cweng@stuy.edu>
2016-12-09 06:39:16 -0500

    schoolwork yay

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -12,30 +12,15 @@ public class Barcode implements Comparable<Barcode>{
     //               or zip contains a non digit
     //               _zip and _checkDigit are initialized.
     public Barcode(String zip) {
-	if (zip.length() != 5){
-	    throw new RuntimeException("Barcode: Length");
-	}
-	try{
-	    Integer a = new Integer(zip);
-	}catch( NumberFormatException e){
-	    throw new RuntimeException("Barcode: Number?");
-	}
-
+	toCode(zip);
 	_zip = zip;
-	_checkDigit = checkSum();
+	_checkDigit = checkSum(_zip);
     }
-
-    // postcondition: Creates a copy of a bar code.
-    public Barcode clone(){
-	Barcode a = new Barcode(_zip);
-	return a;
-    }
-    
 
     // postcondition: computes and returns the check sum for _zip
-    private int checkSum(){
+    private static int checkSum(String zip){
 	int sum = 0;
-	for (int a = 0; a < _zip.length(); a ++){
+	for (int a = 0; a < zip.length(); a++){
 	    sum += (int)_zip.charAt(a);
 	}
 	return sum % 10;
@@ -45,10 +30,19 @@ public class Barcode implements Comparable<Barcode>{
     //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
     public String toString(){
 	String ans = _zip + _checkDigit;
-	return ans += " " + getCode(ans);
+	return ans += " " + toCode(ans);
     }
 
-    public static String getCode(String zip){
+    public static String toCode(String zip){
+	if (zip.length() != 5){
+	    throw new RuntimeException("Barcode: Length?");
+	}
+	try{
+	    Integer a = new Integer(zip);
+	}catch( NumberFormatException e){
+	    throw new RuntimeException("Barcode: Number?");
+	}
+
 	String bar = "|";
 	for (int a  = 0; a < zip.length(); a++){
 	    int num = zip.charAt(a) - '0';
@@ -58,13 +52,25 @@ public class Barcode implements Comparable<Barcode>{
 	return bar;
     }
 
-    public String getZip(){
-	String a = _zip;
-	return a;
+    public static String toZip(String code){
+	if (code.length() != 32){
+	    throw new IllegalArgumentException("Code Length?");
+	}
+	if (code.charAt(0) != '|' || code.charAt(code.length - 1) != '|'){
+	    throw new IllegalArgumentException("Guardrails?");
+	}
+	for (int a = 0; a < code.length(); a++){
+	    if (code.charAt(a) != '|' || code.charAt(a) != ':')
+	}
     }
 
     // postcondition: compares the zip + checkdigit, in numerical order. 
     public int compareTo(Barcode other){
-	return this._zip.compareTo(other.getZip());
+	return this.compareTo(other);
     }
 }
+
+
+
+
+//e.printStackTrace(); for debugging
