bf349147d3e4dcc63a9cab0e9af909162fba97ee
Charles Weng School <cweng@stuy.edu>
2016-12-21 06:39:51 -0500

    work from school

---
+++ temperature/tempGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class tempGUI extends JFrame implements actionListener{
+    private Container pane;
+    private Jbutton bf, bc;
+    private Jlabel lf, lc;
+    
+    
+
+}
