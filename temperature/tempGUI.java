4938c1edfc51ea662923622c0e4080d51662ef68
Charles Weng <cweng@stuy.edu>
2016-12-21 21:55:44 -0500

    compiles

--- temperature/tempGUI.java
+++ temperature/tempGUI.java
@@ -2,11 +2,61 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class tempGUI extends JFrame implements actionListener{
+public class tempGUI extends JFrame implements ActionListener{
     private Container pane;
-    private Jbutton bf, bc;
-    private Jlabel lf, lc;
+    private JButton bf, bc;
+    private JLabel lf, lc;
+    private JTextField tf, tc;
+
+    private void toF(){
+	try{
+	    tf.setText("" + tempConvert.CtoF(Double.parseDouble(tf.getText())));
+	}catch(NumberFormatException e){
+	    tf.setText("0");
+	}
+    }
+
+    private void toC(){
+	try{
+	    tc.setText("" + tempConvert.FtoC(Double.parseDouble(tf.getText())));
+	}catch(NumberFormatException e){
+	    tc.setText("0");
+	}
+    }
     
+    public void actionPerformed(ActionEvent e){
+	
+    }
+
     
+    public tempGUI(){
+	this.setTitle("Temperature Converter");
+	this.setSize(600,400);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	pane = this.getContentPane();
+	pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+	
+	bf = new JButton("Convert to F");
+	bf.addActionListener(this);
+	bf.setActionCommand("toF");
+	bc = new JButton("Convert to C");
+	bc.addActionListener(this);
+	bc.setActionCommand("toC");
+
+	tf = new JTextField(10);
+	tc = new JTextField(10);
+
+	lf = new JLabel("Fahrenheit");
+	lc = new JLabel("Celcius");
+
+	pane.add(lf);
+	pane.add(lc);
+	pane.add(tf);
+	pane.add(tc);
+	pane.add(bf);
+	pane.add(bc);
+    }
 
 }
